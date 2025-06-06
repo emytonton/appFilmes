8a946c8477db45cee5cb190d3dfc0413a0948781
Emilly Paiva <emillypaiva3260@gmail.com>
2025-06-06 10:36:13 -0300

    Add page loading

--- appfilmes/App.js
+++ appfilmes/App.js
@@ -1,17 +1,19 @@
 import React, {useEffect, useState} from 'react';
-import {StyleSheet,Text, View, FlatList} from 'react-native';
+import {StyleSheet,Text, View, FlatList, ActivityIndicator} from 'react-native';
 import api from './src/services/api';
 import Filmes from './src/filmes';
 
 function App(){
 
   const [filmes, setFilmes] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(  () => {
 
     async function loadFilmes() {
       const response = await api.get('r-api/?api=filmes') // rota
       setFilmes(response.data); // salva os filmes
+      setLoading(false);
       
     }
 
@@ -20,7 +22,17 @@ function App(){
 
 
 
-  return (
+  if(loading){
+    return(
+      <View style = {{alignItems: 'center', justifyContent: 'center', flex:1}}> 
+      <ActivityIndicator 
+      color = '#121212'
+      size = {45}
+      />
+      </View>
+    )
+  }else{
+      return (
     <View style={styles.container}>
       <FlatList 
       data = {filmes}
@@ -30,6 +42,10 @@ function App(){
     </View>
   );
   }
+
+
+  }
+
 
 
 const styles = StyleSheet.create({
